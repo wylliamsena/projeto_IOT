7368bb5b21517f74c902844bc37286b5bb9b0684
wylliamsena <wylliamsena80@gmail.com>
2025-09-16 15:10:43 -0300

    12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Livewire\Dashboard;
+use App\Livewire\RegistroIndex;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
+
+Route::get('/index', RegistroIndex::class)->name('registros.index');
